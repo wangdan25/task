5244664c9da3fb0c9991bb8785f012d4509580a7
June.D <you@example.com>
2020-04-14 16:40:30 +0800

    面向对象实现约瑟夫环

--- second_task3.py
+++ second_task3.py
@@ -1,25 +1,40 @@
-class josephus(object):
-    def lastRemaining_solution(self,start_value,step,name):
-        #新建一个保存被淘汰的人的列表
-        delete_name=list()
+#两个对象：环和人
 
-        for i in range(0,len(name)-1):
-            #被淘汰者索引
-            start_value=(start_value+(step-1))%len(name)
-            delete_name.append(name[start_value])
-            name.pop(start_value)
-        final_name=name[0]
+#对象1：环
+class circular(object):
+    def __init__(self,start,step,length):
+        self.start=start
+        self.step=step
+        self.length=length
+        self.out=list()
+    # def creat_sequence(self):
+    #     self.sequence=list(range(0,self.length))
+    def out_num(self):
+        self.sequence=list(range(0,self.length))
+        for i in range(0,self.length):
+            #被淘汰索引
+            self.start=(self.start+(self.step-1))%len(self.sequence)
+            self.out.append(self.sequence[self.start])
+            self.sequence.pop(self.start)  
+        return self.out    
 
-        return (delete_name,final_name)
-
-name=["Lili","Lisa","Aha", "Bob", "Cindy","Marry","Joan","Rose"]
-jos=josephus()
-print("被淘汰的的人和被留下的人分别是：",jos.lastRemaining_solution(2,2,name))
-
-
-
-
+#对象2：人
+class humans(object):
+    def __init__(self,people,show_order,length):
+        self.people=people
+        self.show_order=show_order
+        self.length=length
+    def show_people(self):
+        for i in range(self.length):
+            #利用字典的键输出名字和年龄
+            self.index=self.show_order[i]
+            print("被淘汰的人的名字和年龄是：",self.people[self.index])
             
+people={0:["Lili",12],1:["Lisa",13],2:["Aha",15], 3:["Bob",12], 4:["Cindy",15],5:["Marry",17],6:["Joan",14],7:["Rose",19]}
+length=len(people)
+cir=circular(1,2,length)
+show_order=cir.out_num()
+peo=humans(people,show_order,length)
+peo.show_people()
 
-           
 
