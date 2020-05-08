b855aa73a286893e53ea3bf9c5a2ee18ee9b9d1f
June.D <you@example.com>
2020-05-08 17:41:58 +0800

    添加类型注释

--- joseph_class.py
+++ joseph_class.py
@@ -2,9 +2,10 @@ import copy
 import csv
 import os
 import zipfile
+from typing import List, Iterator
 
 class Person(object):
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         if age<0:
             raise ValueError("The age has to be greater than 0")
@@ -12,7 +13,7 @@ class Person(object):
 
 #约瑟夫环是一个容器，容器的功能有增删查改
 class JosephusRing(object):
-    def __init__(self, reader=None):
+    def __init__(self, reader: List[Person]=None):
         self.start = 0
         self.step = 0
         self._people = []
@@ -20,16 +21,16 @@ class JosephusRing(object):
             for each in reader:
                 self._people.append(each)
 
-    def append(self, obj):
+    def append(self, obj: Person):
         self._people.append(obj)
 
     def pop(self, index):
         self._people.pop(index)
 
-    def query_list(self):
+    def query_list(self) -> List[Person]:
         return self._people
 
-    def next(self):
+    def next(self) -> Iterator[Person]:
         id_ = self.start - 1
         temp = copy.copy(self._people)
         while len(temp):
@@ -39,14 +40,14 @@ class JosephusRing(object):
 
 #定义一个接口对继承类进行约束
 class Reader(object):
-    def read_data(self):
+    def read_data(self) -> List[Person]:
         raise NotImplementedError
 
 class TxtReader(Reader):
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
 
-    def read_data(self):
+    def read_data(self) -> List[Person]:
         reader = []
         with open(self.path, "r") as txt_file:
             for line in txt_file:
@@ -60,37 +61,37 @@ class TxtReader(Reader):
             return reader
 
 class CsvReader(Reader):
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path 
 
-    def read_data(self,):
+    def read_data(self) -> List[Person]:
         reader = []
         with open(self.path, "r") as csv_file:
             data = csv.reader(csv_file)
             for line in data:       
                 name = line[0]
                 try:
-                    age = int(data[1])
+                    age = int(line[1])
                 except ValueError as e:
                     age = 0 
                 reader.append(Person(name, age))
             return reader
 
 class TxtFromZipReader(TxtReader):
-    def __init__(self, path, file_name):
+    def __init__(self, path: str, file_name: str):
         with zipfile.ZipFile(path) as zip_file:
             self.file_path = zip_file.extract(file_name)  
             
-    def read_data(self):
+    def read_data(self) -> List[Person]:
         super().__init__(self.file_path)
         return super().read_data()
 
 class CsvFromZipReader(CsvReader):
-    def __init__(self, path, file_name):
+    def __init__(self, path: str, file_name: str):
         with zipfile.ZipFile(path) as zip_file:
             self.file_path = zip_file.extract(file_name)  
             
-    def read_data(self):
+    def read_data(self) -> List[Person]:
         super().__init__(self.file_path)
         return super().read_data()  
 
@@ -102,7 +103,7 @@ if __name__ == '__main__':
     # jos.append(Person("Joan", 14))
     # jos.append(Person("Rose", 19))
 
-    data = TxtFromZipReader("person.zip","person.txt")
+    data = TxtReader("person.txt")
     reader = data.read_data()
     ring = JosephusRing(reader)
     ring.start = 1
