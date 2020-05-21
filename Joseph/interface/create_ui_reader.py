841f2a6b6470a8c49773fdd7e8f64b0a62165fb7
June.D <you@example.com>
2020-05-21 23:10:28 +0800

    添加ui_reader处理文件

---
+++ Joseph/interface/create_ui_reader.py
@@ -0,0 +1,17 @@
+from Joseph.file_reader import file_reader as rd
+
+class CreateUiReader():
+    def __init__(self, path):
+        self.path = path
+        self.reader = None
+
+    def create_ui_reader(self):
+        file_ = self.path.split(".")
+        if file_[1] == "txt":
+            self.reader = rd.TxtReader(self.path)
+        elif file_[1] == "CSV":
+            self.reader = rd.TxtReader(self.path)
+        else:
+            raise("路径错误")
+        return self.reader.read_data()
+            
