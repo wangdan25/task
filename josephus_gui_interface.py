8fd0b2e12827eb0bf6ea5a71dab720793d8d7583
June.D <you@example.com>
2020-05-25 22:30:12 +0800

    为图形界面添加错误处理机制

--- josephus_gui_interface.py
+++ josephus_gui_interface.py
@@ -1,6 +1,7 @@
 import sys
 from PySide2 import QtGui, QtWidgets, QtCore
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QMessageBox
 from josephus.adapter.gui_interface.mainwindow import Ui_MainWindow
 from josephus.joseph import joseph as jos
 from josephus.adapter import read_data as rd
@@ -13,7 +14,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # 添加槽链接
         self.start_game.clicked.connect(self.show_people)
         self.clear.clicked.connect(self.clear_result)
-        
+ 
+    def josephus_init(self):
+        start = int(self.start.text())
+        if start < 0:
+            QMessageBox.warning(self, 'warning', '起始值不能为负数！')
+            raise ValueError
+        step = int(self.step.text())
+        if step < 1:
+            QMessageBox.warning(self, 'warning', '步进要大于0！')
+            raise ValueError
+        return (start, step)
+
     def show_people(self):
         path = self.path_list.currentText()
         reader = rd.read_data(path)
@@ -21,8 +33,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.original_people.appendPlainText(each.name+" "+str(each.age))
 
         josephus = jos.JosephusRing(reader)
-        josephus.start = int(self.start.text())
-        josephus.step = int(self.step.text())
+        josephus.start,  josephus.step= self.josephus_init()
+        # josephus.step = int(self.step.text())
         for each in josephus:
             self.show_result.appendPlainText(each.name+" "+str(each.age))
         
