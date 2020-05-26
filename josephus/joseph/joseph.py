79463b3eac0597a8ccd30f407d722abb082feda7
June.D <you@example.com>
2020-05-26 16:51:36 +0800

    将people属性去私有化

--- josephus/joseph/joseph.py
+++ josephus/joseph/joseph.py
@@ -6,32 +6,32 @@ class JosephusRing(object):
     def __init__(self, reader: List[ps.Person]=None):
         self.start = 0
         self.step = 0
-        self._people = []
+        self.people = []
         if reader:
             for each in reader:
-                self._people.append(each)
+                self.people.append(each)
     
     def __len__(self):
-        return len(self._people)
+        return len(self.people)
 
     def append(self, obj: ps.Person):
-        self._people.append(obj)
+        self.people.append(obj)
 
     def pop(self, index):
-        self._people.pop(index)
+        self.people.pop(index)
 
     def query_list(self) -> List[ps.Person]:
-        return self._people
+        return self.people
 
     def __iter__(self):
         return self
 
     def __next__(self) -> Iterator[ps.Person]:
-        if not self._people:
+        if not self.people:
             raise StopIteration
         id_ = self.start - 1
-        id_ = (id_ + (self.step-1)) % len(self._people)
-        ret = self._people.pop(id_)
+        id_ = (id_ + (self.step-1)) % len(self.people)
+        ret = self.people.pop(id_)
         return ret
 
 #定义一个接口对继承类进行约束
